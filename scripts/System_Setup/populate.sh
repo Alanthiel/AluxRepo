f038c4e925c176a868c763f17b7789fb4e846d6a
Alux-Alpha <Nehal.GS@protonmail.ch>
2020-12-31 16:06:13 +0530

    populate.sh update

--- scripts/System_Setup/populate.sh
+++ scripts/System_Setup/populate.sh
@@ -1,6 +1,6 @@
 pacman -Syyu --noconform --needed
-pacman -S alacritty fish qtile rofi code axel bmenu android-tools xsecurelock broot discord vlc gufw krita qbittorrent lutris seahorse gvim cronie dpkg base-devel mlocate gnome-keyring inxi neofetch lightdm-settings less lxappearance polkit-gnome ranger rsync speedtest-cli tree bpytop google-cloud-sdk --noconfirm --needed
-pamac install mailspring nerd-fonts-complete nerd-fonts-hermit picom-jonaburg-git clion pycharm-professional
+pacman -S alacritty fish qtile rofi code axel bmenu android-tools xsecurelock broot discord vlc gufw krita qbittorrent lutris seahorse gvim cronie dpkg base-devel mlocate gnome-keyring inxi neofetch lightdm-settings less lxappearance polkit-gnome ranger rsync speedtest-cli tree bpytop --noconfirm --needed
+pamac install mailspring nerd-fonts-complete nerd-fonts-hermit picom-jonaburg-git clion pycharm-professional google-cloud-sdk
 #mailspring nerd-fonts-complete nerd-fonts-hermit picom-jonaburg-git
 
 #get into alux
@@ -19,11 +19,11 @@ cp -rf ~alux/workspace/repo/.git/* ~alux/AluxRepo
 #setanaconda
 conda init fish
 conda create --name Encr
-conda install --name Encr python==3.7 pycryptodome sqlite3 json scrypt pyqt -y
+conda install --name Encr python==3.7 pycryptodome scrypt pyqt -y
 conda create --name software_base
-conda install --name software_base python==3.7 pyqt tk pycryptodome pyinstaller pandas numpy sqlite3
+conda install --name software_base python==3.7 pyqt tk pycryptodome pandas numpy
 conda create --name tensor_base
-conda install --name tensor_base python=3.7 tensorflow-gpu keras-gpu pandas numpy seaborn pytinstaller scipy sqlite3 matplotlib
+conda install --name tensor_base python==3.7 tensorflow-gpu keras-gpu pandas numpy seaborn pytinstaller scipy matplotlib
 
 #lateral setups
 mv ~alux/workspace/PycharmProjects ~alux/PycharmProjects
@@ -31,3 +31,6 @@ mv ~alux/workspaces/CLionProjects ~alux/ClionProjects
 mv ~alux/workspace/'2FA recovery codes' ~alux/'2FA recovery codes'
 mv ~alux/workspace/.aws ~alux/.aws
 mv ~alux/workspace/.kaggle ~alux/.kaggle
+mv ~alux/workspace/.git-credentials ~alux/.git-credentials
+
+pip install kaggle --yes
