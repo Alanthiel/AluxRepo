8f890a42896ac7c53fea8dee5648085e063ba5a1
Alux-Alpha <Nehal.GS@protonmail.ch>
2021-07-11 19:41:30 +0530

    repo update

--- scripts/System_Setup/conda_set.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-conda install --name Encr python==3.7 pycryptodome  json scrypt pyqt -y
-conda install --name software_base python==3.7 pyqt tk pycryptodome pyinstaller pandas numpy 
-conda install --name tensor_base python==3.7 tensorflow-gpu keras-gpu pandas numpy seaborn  scipy  matplotlib
