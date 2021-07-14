ae18cac5e3e31059d239f394dd5d3e4d57ed8352
Alux-Alpha <Nehal.GS@protonmail.ch>
2021-07-14 19:04:23 +0530

    new powerline

--- Dotfiles/.config/fish/config.fish
+++ Dotfiles/.config/fish/config.fish
@@ -6,88 +6,6 @@ eval /home/alux/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # >>> personal settings >>>
 
 set fish_greeting
-
-
-# >>> Shashimi Prompt >>>
-#
-# Credit to : https://github.com/isacikgoz/sashimi/blob/master/fish_prompt.fish
-
-function fish_prompt
-  set -l last_status $status
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -g red (set_color -o red)
-  set -g blue (set_color -o blue)
-  set -l green (set_color -o green)
-  set -g normal (set_color normal)
-
-  set -l ahead (_git_ahead)
-  set -g whitespace ' '
-
-  if test $last_status = 0
-    set initial_indicator "$green◆"
-    set status_indicator "$normal❯$cyan❯$green❯"
-  else
-    set initial_indicator "$red✖ $last_status"
-    set status_indicator "$red❯$red❯$red❯"
-  end
-  set -l cwd $cyan(basename (prompt_pwd))
-
-  if [ (_git_branch_name) ]
-
-    if test (_git_branch_name) = 'master'
-      set -l git_branch (_git_branch_name)
-      set git_info "$normal git:($red$git_branch$normal)"
-    else
-      set -l git_branch (_git_branch_name)
-      set git_info "$normal git:($blue$git_branch$normal)"
-    end
-
-    if [ (_is_git_dirty) ]
-      set -l dirty "$yellow ✗"
-      set git_info "$git_info$dirty"
-    end
-  end
-
-  # Notify if a command took more than 5 minutes
-  if [ "$CMD_DURATION" -gt 300000 ]
-    echo The last command took (math "$CMD_DURATION/1000") seconds.
-  end
-
-  echo -n -s $initial_indicator $whitespace $cwd $git_info $whitespace $ahead $status_indicator $whitespace
-end
-
-function _git_ahead
-  set -l commits (command git rev-list --left-right '@{upstream}...HEAD' ^/dev/null)
-  if [ $status != 0 ]
-    return
-  end
-  set -l behind (count (for arg in $commits; echo $arg; end | grep '^<'))
-  set -l ahead  (count (for arg in $commits; echo $arg; end | grep -v '^<'))
-  switch "$ahead $behind"
-    case ''     # no upstream
-    case '0 0'  # equal to upstream
-      return
-    case '* 0'  # ahead of upstream
-      echo "$blue↑$normal_c$ahead$whitespace"
-    case '0 *'  # behind upstream
-      echo "$red↓$normal_c$behind$whitespace"
-    case '*'    # diverged from upstream
-      echo "$blue↑$normal$ahead $red↓$normal_c$behind$whitespace"
-  end
-end
-
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
-
-# <<< Shashimi Prompt <<< 
-
-
 alias enter_build="cd /mnt/InSpace/Systemd/build"
 alias enter_systemd="cd /mnt/InSpace/Systemd"
 alias enter_adb="cd /mnt/InSpace/Systemd/build/android/"
@@ -121,6 +39,14 @@ set qconfig "/home/alux/AluxRepo/Dotfiles/.config/qtile/config.py"
 
 export SAM_CLI_TELEMETRY=0
 set EDITOR 'vim'
+
+# >>> promt settings:
+
+set SPACEFISH_CONDA_SHOW false 
+set SPACEFISH_EXIT_CODE_SHOW true
+set SPACEFISH_DIR_TRUNC 1
+set SPACEFISH_USER_SHOW truei
+set SPACEFISH_PROMPT_SEPARATE_LINE false
 
 function restart_wifi
     wifi off
